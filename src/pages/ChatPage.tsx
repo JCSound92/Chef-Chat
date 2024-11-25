51a343f1fbdeabb45794b4b3b462e8ab23ea6392
JCSound92 <jonahcohensound@gmail.com>
2024-11-25 12:00:10 -0600

    weird chat counting problem

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { ChatHistory } from '../components/ChatHistory';
-import { useStore } from '../store';
 
 export function ChatPage() {
-  const { chatHistory } = useStore();
-
   return (
     <div className="flex flex-col h-full">
       <ChatHistory />
